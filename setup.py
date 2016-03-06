acb7ab636c5962cb8de3dc486f774d09da6a5fb0
Hong Minhee <hongminhee@member.fsf.org>
2016-03-07 06:08:47 +0900

    Fix build broken by sftpserver/distribute

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ pyversion_requires = {
 
 tests_require = [
     'pytest >= 2.5.0',
-    'sftpserver == 0.2py3',  # https://github.com/spoqa/sftpserver/releases
+    'sftpserver == 0.2setuptools',
+    # FIXME: https://github.com/spoqa/sftpserver/releases
     'iso8601 >= 0.1.10',
     'redis',
     'pytest-cov'
